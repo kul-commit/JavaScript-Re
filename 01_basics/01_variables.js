cab3d9065da3467ec97b6b8f4a71237659dd719e
kuldeep yadav <kulcodes@gmail.com>
2026-09-20 00:33:01 +0000

    01_variables completed

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 138060
+let accountEmail = "Kul@gmail.com"
+var accountPassword = "vyrus12"
+accountCity = "Delhi"
+
+let accountState;
+
+//accountId= 2.  not allowed
+
+accountEmail=("cyrus@gmail.com")
+accountPassword = "22111322"
+accountCity="neew york"
+
+// console.log(accountId);
+// console.log(accountEmail);
+
+
+/*
+preferred not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountPassword, accountCity, accountEmail, accountState]);
