9c4876379492af3e515b8a7e6fda28523cb5c433
kuldeep yadav <kulcodes@gmail.com>
2026-09-20 00:50:12 +0000

    Datatypes completed

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";// treat all the JS code as newer version
+
+// alert("hello"). we are using nodejs , not browser that is why this will not work there is another way
+
+
+let name = "cyrus"
+let age = 21
+let isLoggedIn= false
+
+
+// types of datatype in js
+/*  number- 2 to power 53
+    bigint - used for big no.
+    string => always in  single and double quotes '' ""
+    boolean => true/ false
+    null => standalone value
+    undefined => when you do not define any value
+    symbol =>  when we have to tak about unique 
+
+    object
+    */
+
+
+    console.log(typeof "kul")
+    console.log(typeof null) // object
+    console.log(typeof undefined) // undefined
