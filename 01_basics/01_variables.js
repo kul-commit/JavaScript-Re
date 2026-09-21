07e142da60de34c8231591e528ea94fe0358c521
kuldeep yadav <kulcodes@gmail.com>
2026-09-21 16:35:28 +0000

    variable practice

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -22,3 +22,16 @@ because of issue in block scope and functional scope
 
 
 console.table([accountId, accountPassword, accountCity, accountEmail, accountState]);
+
+
+// create 3 variable
+
+let firstName = "Cyrus"
+let age = 22 
+let city = "New Delhi"
+
+console.log(firstName);
+console.log(age);
+console.log(city);
+
+console.log("Hi my name is ",firstName +" I am " ,age ,"old" + "I live in" ,city );
