d3c36b254363a15e646c8daf91228b2e4e14bf83
kuldeep yadav <kulcodes@gmail.com>
2026-09-21 16:24:01 +0000

    comparison done

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -5,13 +5,20 @@
 // console.log(3==2);
 // console.log(3!=2);
 
+ // avoid these conversions below
+
+// console.log( "89" > 1);
+// console.log("02" < 4);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null<0);
+// console.log(null>=0);
+// console.log(null<=0);
+
+console.log(undefined==1);
 
 
-console.log( "89" > 1);
-console.log("02" < 4);
+// === is used for strict check
 
-console.log(null>0);
-console.log(null==0);
-console.log(null<0);
-console.log(null>=0);
-console.log(null<=0);
+console.log("2" === 2);
