fe33fbf483e3ce0b80e9ca98cc9e7ae1872a44c8
kuldeep yadav <kulcodes@gmail.com>
2026-09-21 16:39:52 +0000

    ques 2 done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -35,3 +35,11 @@ console.log(age);
 console.log(city);
 
 console.log("Hi my name is ",firstName +" I am " ,age ,"old" + "I live in" ,city );
+
+
+//Create a variable score with value 85. Change its value to 95 and print it.
+
+let score = 85
+score = 95
+
+console.log(score)
