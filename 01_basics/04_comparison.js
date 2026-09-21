e81952c64ff7dacfda36a71179d8d5f7fe88bfcf
kuldeep yadav <kulcodes@gmail.com>
2026-09-21 16:19:35 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,17 @@
+// console.log(3>2);
+// console.log(3>=2);
+// console.log(3<2);
+// console.log(3<=2);
+// console.log(3==2);
+// console.log(3!=2);
+
+
+
+console.log( "89" > 1);
+console.log("02" < 4);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null<0);
+console.log(null>=0);
+console.log(null<=0);
